ff57e844be327584e945e68e8eaed6769853c02b
jeffr22 <jeff@btcom.us>
2024-01-15 09:51:57 -0500

    Updated the popup form

--- root/static/js/rcCrewGrid.js
+++ root/static/js/rcCrewGrid.js
@@ -4,6 +4,11 @@
 
 function RcCrewGrid(staffJson) {
     let self = this;
+    self.username = ko.observable("");
+    self.useremail = ko.observable("");
+    self.title = ko.observable("");
+    self.position = "";
+    self.bookDate = "";
     self.aRcCrews = ko.observableArray([]);
     //Loop through the staff data to create an array of staff objects
     let staffArray = JSON.parse(staffJson);
@@ -14,21 +19,53 @@ function RcCrewGrid(staffJson) {
 
     self.editCapt = function(row,evt){
         console.log("Captain on %s",row.date);
+        self.position="Captain";
+        self.date=row.date;
+        self.title("Booking for: " + self.position + " on: " + self.date);
+        $('#myModal').show();
     }
     self.editAsst = function (row, evt) {
         console.log("Asst on %s", row.date);
+        self.position = "Assistant";
+        self.date = row.date;
+        self.title("Booking for: " + self.position + " on: " + self.date);
+        $('#myModal').show();
     }
     self.editS11 = function (row, evt) {
         console.log("S11 on %s", row.date);
+        self.position = "Safety Boat 1A";
+        self.date = row.date;
+        self.title("Booking for: " + self.position + " on: " + self.date);
+        $('#myModal').show();
     }
     self.editS12 = function (row, evt) {
         console.log("S12 on %s", row.date);
+        self.position = "SBoat 1B";
+        self.date = row.date;
+        self.title("Booking for: " + self.position + " on: " + self.date);
+        $('#myModal').show();
     }
     self.editS21 = function (row, evt) {
         console.log("S21 on %s", row.date);
+        self.position = "SBoat 2A";
+        self.date = row.date;
+        self.title("Booking for: " + self.position + " on: " + self.date);
+        $('#myModal').show();
     }
     self.editS22 = function (row, evt) {
         console.log("S22 on %s", row.date);
+        self.position = "SBoat 2B";
+        self.date = row.date;
+        self.title("Booking for: " + self.position + " on: " + self.date);
+        $('#myModal').show();
+    }
+
+    self.formSave = function(a,b){
+        $('#myModal').hide();
+    }
+
+    self.formCancel = function (a, b) {
+        $('#myModal').hide();
     }
 }
 
