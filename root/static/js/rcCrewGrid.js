5a3e9667015032b8f2b934bb79effbd3e3552700
jeffr22 <jeff@btcom.us>
2024-01-15 08:25:00 -0500

    Added code to handle button events

--- root/static/js/rcCrewGrid.js
+++ root/static/js/rcCrewGrid.js
@@ -11,6 +11,25 @@ function RcCrewGrid(staffJson) {
         let tmpObj = new RcCrew(staffArray[k]);
         self.aRcCrews.push(tmpObj);
     }
+
+    self.editCapt = function(row,evt){
+        console.log("Captain on %s",row.date);
+    }
+    self.editAsst = function (row, evt) {
+        console.log("Asst on %s", row.date);
+    }
+    self.editS11 = function (row, evt) {
+        console.log("S11 on %s", row.date);
+    }
+    self.editS12 = function (row, evt) {
+        console.log("S12 on %s", row.date);
+    }
+    self.editS21 = function (row, evt) {
+        console.log("S21 on %s", row.date);
+    }
+    self.editS22 = function (row, evt) {
+        console.log("S22 on %s", row.date);
+    }
 }
 
 
